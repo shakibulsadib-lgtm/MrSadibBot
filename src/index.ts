f7ea244e7c769ba6134b46abbaa73aaf947be245
shakibulsadib-lgtm <shakibulsadib@gmail.com>
2026-08-22 20:36:23 +0600

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,7 @@ const {
 
 const CONFIG = {
   host: process.env.MC_HOST || 'MrSadib.aternos.me',
-  port: Number(process.env.MC_PORT || 25565),
+  port: Number(process.env.MC_PORT || 54276),
 
   username: process.env.BOT_USERNAME || 'MyBot',
 
